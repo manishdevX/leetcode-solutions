a3979702dd8a529bd4099c912edeab586f071b45
Manish Singh <58559076+manishdevX@users.noreply.github.com>
2024-11-24 03:24:06 +0530

    Solved leetcode array problems

---
+++ find-triangular-sum-of-an-array.cpp
@@ -0,0 +1,69 @@
+// LINK - https://leetcode.com/problems/find-triangular-sum-of-an-array/description/
+
+// n = size of nums
+
+// Solution 1 TC = O(n^2), SC = O(n)
+class Solution
+{
+public:
+    int triangularSum(vector<int> &nums)
+    {
+        while (nums.size() > 1)
+        {
+            int n = nums.size() - 1;
+            vector<int> newNums(n);
+            for (int i = 0; i < n; i++)
+            {
+                newNums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            nums = newNums;
+        }
+        return nums[0];
+    }
+};
+
+// Solution 2 TC = O(n^2), SC = O(1)
+class Solution
+{
+public:
+    int triangularSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        while (n > 1)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            n--;
+        }
+        return nums[0];
+    }
+};
+
+// Solution 3 TC = O(n^2), SC = O(n)
+class Solution
+{
+public:
+    int triangularSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+        {
+            q.push(nums[i]);
+        }
+        while (q.size() > 1)
+        {
+            for (int i = 0; i < q.size() - 1; i++)
+            {
+                int a = q.front();
+                q.pop();
+                int b = q.front();
+                q.push((a + b) % 10);
+            }
+            q.pop();
+        }
+        return q.front();
+    }
+};
---
+++ first-missing-positive.cpp
@@ -0,0 +1,45 @@
+// LINK - https://leetcode.com/problems/first-missing-positive/description/
+
+// n = size of nums
+
+// Solution 1 TC = O(n), SC = O(1)
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        int n = nums.size();
+        bool contains1 = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == 1)
+            {
+                contains1 = true;
+            }
+            if (nums[i] <= 0 || nums[i] > n)
+            {
+                nums[i] = 1;
+            }
+        }
+        if (!contains1)
+        {
+            return 1;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            int ind = abs(nums[i]) - 1;
+            if (nums[ind] > 0)
+            {
+                nums[ind] *= -1;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > 0)
+            {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+};
