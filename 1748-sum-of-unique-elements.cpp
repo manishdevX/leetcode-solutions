029166d24110ea4a9feaf1e17201c6329ee526ab
Manish Singh <58559076+manishdevX@users.noreply.github.com>
2026-02-21 21:14:55 +0530

    Renamed the files

--- 1748-sum-of-unique-elements.cpp
+++ 1748-sum-of-unique-elements.cpp
@@ -2,7 +2,7 @@
 
 // n = size of nums
 
-// Solution 1 TC = O(n^2), SC = O(1)
+// Solution 1, TC = O(n^2), SC = O(1)
 class Solution
 {
 public:
@@ -20,7 +20,7 @@ public:
     }
 };
 
-// Solution 2 TC = O(n), SC = O(n)
+// Solution 2, TC = O(n), SC = O(n)
 class Solution
 {
 public:
--- 961-n-repeated-element-in-size-2n-array.cpp
+++ 961-n-repeated-element-in-size-2n-array.cpp
@@ -2,7 +2,7 @@
 
 // n = size of nums
 
-// Solution 1 TC = O(n^2), SC = O(1)
+// Solution 1, TC = O(n^2), SC = O(1)
 class Solution
 {
 public:
@@ -21,7 +21,7 @@ public:
     }
 };
 
-// Solution 2 TC = O(n), SC = O(n)
+// Solution 2, TC = O(n), SC = O(n)
 class Solution
 {
 public:
@@ -42,7 +42,7 @@ public:
     }
 };
 
-// Solution 3 TC = O(nlogn), SC = O(1)
+// Solution 3, TC = O(nlogn), SC = O(1)
 class Solution
 {
 public:
